83f12a14d323b462cfb62af47ed86435dff6fe68
zaibasamreen <zaibasarowar107@gmail.com>
2021-09-17 10:11:04 +0530

    third commit

--- script.js
+++ script.js
@@ -4,12 +4,11 @@
 
 function add(){
     const Input = document.getElementById("task");
-    const field = document.getElementById("inputfield");
-    const newElement = document.createElement("h3")
+   const field = document.getElementById("inputfield");
+   const newElement = document.createElement("h3")
     newElement.innerHTML=Input.value;
     field.appendChild(newElement);
-
-}
+    }
 
 function remove(){
     const e = document.getElementsByTagName("h3");
